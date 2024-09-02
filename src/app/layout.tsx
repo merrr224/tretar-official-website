2a6e7478f84a7a1e88e1966d7ace2c3a3825db18
merrr224 <mrrr3348@gmail.com>
2024-09-03 01:44:09 +0900

    fix: google analyticsを追加、metaタグ追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -6,7 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'TRETÅR | ボードゲームスペース',
-  description: 'Boardgame space',
+  description: 'TRETÅR（トレートール）：上野駅徒歩３分のボードゲームスペース',
+  keywords: ['Boardgame', 'ボードゲーム', 'ボドゲ', '上野'],
+  metadataBase: new URL('https://tre-tar.com'),
+  openGraph: {
+    images: '/gallery/IMG_5012.jpeg',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-3V6JSZGNVS" />
     </html>
   )
 }
