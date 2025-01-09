55a68af27240818269120824ea93b015e55cc32c
merrr224 <mrrr3348@gmail.com>
2025-01-09 23:28:20 +0900

    fix: サイトタイトルを微修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール TRETAR）',
+  title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール）',
   description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームスペースです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
   keywords: ['Boardgame', 'ボードゲーム', 'ボドゲ', 'ボドゲカフェ', 'ボードゲームスペース','アナログゲーム', '上野', '東京'],
   metadataBase: new URL('https://tre-tar.com'),
   openGraph: {
-    title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール TRETAR）',
+    title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール）',
     description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームスペースです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
     siteName: 'TRETAR',
     images: '/gallery/IMG_5012.jpeg',
