bac410bfb95ae3e7b5a72c568efa659664d63897
merrr224 <mrrr3348@gmail.com>
2025-01-22 22:25:31 +0900

    fix: サイトのdescriptionを微修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール）',
-  description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームスペースです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
-  keywords: ['Boardgame', 'ボードゲーム', 'ボドゲ', 'ボドゲカフェ', 'ボードゲームスペース','アナログゲーム', '上野', '東京'],
+  description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームカフェです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
+  keywords: ['Boardgame', 'Boardgame cafe', 'Boardgame space', 'ボードゲーム', 'ボドゲ', 'ボドゲカフェ', 'ボードゲームカフェ', 'ボードゲームスペース', 'アナログゲーム', '上野', '東京'],
   metadataBase: new URL('https://tre-tar.com'),
   openGraph: {
     title: 'TRETÅR - 上野駅から一番近くて安いボードゲームカフェ（トレートール）',
-    description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームスペースです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
+    description: 'TRETÅR（トレートール TRETAR） | 上野駅入谷口徒歩3分のボードゲームカフェです。こだわりの空間でゆったりとしたボードゲーム時間をお楽しみください。',
     siteName: 'TRETAR',
     images: '/gallery/IMG_5012.jpeg',
     locale: 'ja_JP',
